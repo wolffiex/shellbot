7276c36a7079babe4af948cad5216dbe1c8958df
Adam Wolff <adam@electriccapital.com>
2023-05-08 19:10:01 +0000

    Script improvements

--- shellbot.sh
+++ shellbot.sh
@@ -6,26 +6,62 @@ script_dir="$(dirname "$script_path")"
 binary_name="$script_dir/target/release/shellbot"
 
 console_width=$(tput cols)
-# Check if stdin is empty
-if [ -t 0 ]; then
-  # If stdin is empty, create a temporary file and open it in the editor
-  tmpfile=$(mktemp)
-  trap "rm -f $tmpfile" EXIT
-  ${EDITOR} "$tmpfile"
-  echo "🧑 $USER"
-  cat "$tmpfile" | fold -s -w "$console_width"
-  # Use edited file as input source
-  input_source="$tmpfile"
-else
-  # Otherwise, use stdin as input source
-  input_source="/dev/stdin"
-fi
-
+startfile=$(mktemp)
+trap "rm -f $startfile" EXIT
+transcript=$(mktemp)
+trap "rm -f $transcript" EXIT
+${EDITOR} "$startfile"
 
 separator="───  ⋅ ∙ ∘ ༓ ∘ ⋅ ⋅  ───"
 padding_width=$(( (console_width - ${#separator}) / 2 ))
 padded_separator=$(printf "%*s%s" ${padding_width} '' "${separator}")
-echo "${padded_separator}"
-echo -e "🤖 shellbot"
-# Invoke the binary with the input source and pipe the output through fold
-"$binary_name" < "$input_source" | fold -s -w "$console_width"
+user_input=$(cat "$startfile")
+echo -e "🧑 $USER"
+echo "$user_input" | fold -s -w "$console_width"
+while true; do
+    echo "===USER===" >> "$transcript"
+    echo "$user_input" >> "$transcript"
+    echo "${padded_separator}"
+    echo -e "🤖 shellbot"
+    echo "===ASSISTANT===" >> "$transcript"
+    response_file=$(mktemp)
+    # Use 'tee' to simultaneously capture the output and send it to 'fold'
+    "$binary_name" < "$transcript" | tee "$response_file" | fold -s -w "$console_width"
+
+    cat "$response_file" >> "$transcript"
+    rm "$response_file"
+
+    read -p "Continue? [No/yes/log]: " choice
+    choice=${choice:-no}
+
+    case $choice in
+      [nN]* | no | No | NO) exit;;
+      [lL]* | log | Log | LOG) 
+          read -p "Enter a filename: " logfile
+          cp "$transcript" "$logfile"
+          exit
+          ;;
+      [yY]* | yes | Yes | YES) 
+          printf "\033[1A"  # Move cursor one line up
+          printf "\033[K"   # Clear the line
+          echo "${padded_separator}"
+          ;;
+      *) 
+          echo "Invalid input"
+          exit 1
+          ;;
+    esac
+
+    echo -e "🧑 $USER"
+
+    input=""
+    while true; do
+        read -r -e line
+        if [ -z "$line" ]; then
+            break
+        fi
+        input+="${line}"$'\n'
+    done
+
+    user_input=$input
+done
